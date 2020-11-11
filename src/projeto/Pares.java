99268345c133a9fb9d2489d5445aab09513bf5c2
jose1gcm <josegcmoraes@gmail.com>
2020-11-11 09:16:54 -0300

    Adicionando o arquivo Pares, responsável por definir um mapa de veículos

---
+++ src/projeto/Pares.java
@@ -0,0 +1,25 @@
+
+package projeto;
+
+/**
+ *
+ * @author jose
+ */
+public class Pares {
+    private int chave;
+    private int quantidade;
+
+    public int getChave() {
+        return chave;
+    }
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
