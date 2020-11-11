37279708a3e4543607e09048290e1b8a72e4eb50
jose1gcm <josegcmoraes@gmail.com>
2020-11-11 09:14:19 -0300

    Adicionando o arquivo de  decodificar rota

---
+++ src/projeto/DecodificarRota.java
@@ -0,0 +1,16 @@
+
+package projeto;
+
+import java.util.ArrayList;
+
+class DecodificarRota{
+    ArrayList<Integer> cliente_adicionado=new ArrayList<Integer>();
+
+    public ArrayList<Integer> getCliente_adicionado() {
+        return cliente_adicionado;
+    }
+    public void setCliente_adicionado(ArrayList<Integer> cliente_adicionado) {
+        this.cliente_adicionado = cliente_adicionado;
+    }
+   
+}
