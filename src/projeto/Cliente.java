87c01cd4796d2f2ea4dcb26ddbb8cf038aa5eb92
jose1gcm <josegcmoraes@gmail.com>
2020-11-11 09:10:19 -0300

    Adicionado o arquivo de Clientes

---
+++ src/projeto/Cliente.java
@@ -0,0 +1,37 @@
+
+package projeto;
+
+public class Cliente {
+    private int id;
+    private int x;
+    private int y;
+    private int qtde_recursos;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getQtde_recursos() {
+        return qtde_recursos;
+    }
+    public void setQtde_recursos(int qtde_recursos) {
+        this.qtde_recursos = qtde_recursos;
+    }
+}
