35ae3a492e77492c758d268c068d5c2f4439704c
jose1gcm <josegcmoraes@gmail.com>
2020-11-11 09:12:17 -0300

    Adicionado o arquivo de veículos

---
+++ src/projeto/Veiculo.java
@@ -0,0 +1,37 @@
+
+package projeto;
+
+public class Veiculo {
+    private int capacidade;
+    private float custo_operacional;
+    private int qtde_veiculo;
+    private float qtde_emissao;
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public float getCusto_operacional() {
+        return custo_operacional;
+    }
+    public void setCusto_operacional(float custo_operacional) {
+        this.custo_operacional = custo_operacional;
+    }
+
+    public int getQtde_veiculo() {
+        return qtde_veiculo;
+    }
+    public void setQtde_veiculo(int qtde_veiculo) {
+        this.qtde_veiculo = qtde_veiculo;
+    }
+
+    public float getQtde_emissao() {
+        return qtde_emissao;
+    }
+    public void setQtde_emissao(float qtde_emissao) {
+        this.qtde_emissao = qtde_emissao;
+    }
+}
